fb72121b8756fe46c160ef6e32bfd153c809ccec
Beatus Simon <beatussimon49@gmail.com>
2025-03-26 10:27:45 +0300

    Update custom admin

--- dsmtimes/settings.py
+++ dsmtimes/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dsmtimes-1.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
